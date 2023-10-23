08bb1bf6fd6c85c48de9bd1884ab39f92c8136c6
nurbekalpisbay <148778632+nurbekalpisbay@users.noreply.github.com>
2023-10-23 20:06:55 +0600

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,133 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+//        byte a = 127;
+//        short b = 2743;
+//        int c = 13;
+//        long d = 12345456;
+//        float e = 2345456657f;
+//        char g = 'g';
+//        boolean t = 5 == 5;
+//        String text = "" + "" + 5 + "";
+//        System.out.print(text + "text");
+//        int aa = -10+10;
+//        System.out.println("aa");
+//
+//        int hy = 9;
+//        int hu = 3;
+//        double hi = 1/3;
+//        int ho = 1;
+//        System.out.println(hy-hu/hi+ho);
+
+//        int a=-7;
+//        int b=6;
+//
+//        int c= Math.max(a,b);
+//        System.out.print(c);
+//
+//        int d=Math.min(a,b);
+//        System.out.print(d);
+
+//        int b=3;
+//        int v=6;
+//        int n=7;
+//        int m=2;
+//
+//        int h= Math.max(b,v);
+//        int l= Math.max(h,n);
+//        int a= Math.max(l,m);
+//        System.out.println(a);
+//
+//        int q= Math.min(b,v);
+//        int w= Math.min(q,n);
+//        int e= Math.min(w,m);
+//        System.out.println(e);
+//
+//        int t= Math.max(Math.max(b,v),Math.max(n,m));
+//        System.out.println(t);
+//
+//        int y= Math.min(Math.min(b,v),Math.min(n,m));
+//        System.out.print(y);
+
+//        double rootof = Math.sqrt(64);
+//        System.out.println(rootof);
+//
+//        double powdareje = Math.pow(3, 3);
+//        System.out.println(powdareje);
+
+//        double a = Math.pow(2022, 3);
+//        double b = Math.pow(2021, 3);
+//        double c = Math.pow(2023, 2);
+//        double v= Math.pow(2022,2);
+//        double d = 1;
+//
+//        System.out.print((a + b) / (c + v + d));
+
+//        int a= 23456;
+//        // Goal : jeke jeke sandarin wiraru
+//
+//        int first_digit = a/10000;
+//        int second_digit = a/1000%10;
+//        int third_digit = a/100%10;
+//        int fourth_digit = a%100/10;
+//        int fifth_digit = a%10;
+//        System.out.print(first_digit+" "+second_digit+" "+third_digit+" "+fourth_digit+" "+fifth_digit);
+
+//        int a= 234;
+//        int apta= 7;
+//
+//        System.out.println(a/apta);
+//
+//        int v= 43;
+//        int bir= v/10;
+//        int eki= v%10;
+//
+//
+//        System.out.println(bir+" "+eki);
+//        System.out.println(bir+eki);
+//        System.out.println(bir*eki);
+//
+//        int j= 987;
+//        int uw= j/10;
+//        int tort= j%10;
+//
+//        System.out.println(uw+ " " +tort);
+
+//        int f= 367;
+//        int bir= f/100;
+//        int eki= f/10%10;
+//        int uw= f%10;
+//7
+//      System.out.println(bir+" "+eki+" "+uw);
+//        System.out.println(bir+eki+uw);
+//        System.out.println(bir*eki*uw);
+//8
+//        System.out.println(bir+""+eki+""+uw);
+//        System.out.println(uw+""+eki+""+bir);
+//        System.out.println(eki+""+uw+""+bir);
+//        System.out.println(eki+""+bir+""+uw);
+//9
+//        int a= 789;
+//        int bir= a/100;
+//        int eki= a/10%10;
+//        int uw= a%10;
+//
+//        System.out.println(eki+""+bir+""+uw);
+//        System.out.println(eki+""+uw+""+bir);
+//        System.out.println(bir+""+eki+""+uw);
+//        System.out.println(bir+""+uw+""+eki);
+//        System.out.println(uw+""+bir+""+eki);
+//        System.out.println(uw+""+eki+""+bir);
+
+//        int a= 1234;
+//        int b= 1234/1000;
+//        int c= 1234/100%10;
+//        int d= 1234/10%10;
+//        int e= 1234%10;
+//
+//        System.out.println(b+c+d+e);
+//        System.out.println(b*c*d*e);
+
+    }
+}
